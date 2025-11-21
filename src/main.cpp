bef2934888e0258889ef797ab995e583a929d36f
Lindsay Lim <limlindsayj@gmail.com>
2025-11-21 15:29:05 -0800

    broken lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include "driver/i2s.h"
 #include <arduinoFFT.h>
+#include <LiquidCrystal_I2C.h>
 
 #define I2S_WS   25    // LRCLK
 #define I2S_SCK  26    // BCLK
 #define I2S_SD   33    // DOUT
 
 #define SAMPLE_RATE   16000
-#define FFT_SIZE      1024     // must be power of 2!
+#define FFT_SIZE      1024
 
 arduinoFFT FFT = arduinoFFT();
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 double vReal[FFT_SIZE];
 double vImag[FFT_SIZE];
@@ -17,7 +19,9 @@ double vImag[FFT_SIZE];
 void setup() {
   Serial.begin(9600);
   delay(500);
-
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hello ESP32!");
   i2s_config_t i2s_config = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = SAMPLE_RATE,
@@ -41,13 +45,12 @@ void setup() {
 
   Serial.println("Microphone FFT pitch detection started!");
 }
+
 String freqToNote(float freq) {
     if (freq <= 0) return "None";
 
-    // MIDI note number
     int midi = round(69 + 12 * log(freq / 440.0) / log(2.0));
 
-    // Note names
     const char* noteNames[] = {
         "C", "C#", "D", "D#", "E", "F",
         "F#", "G", "G#", "A", "A#", "B"
@@ -59,29 +62,25 @@ String freqToNote(float freq) {
     String note = String(noteNames[noteIndex]) + String(octave);
     return note;
 }
+
 void loop() {
   int32_t sample32;
   size_t bytes_read;
 
-  // === Read samples from microphone ===
   for (int i = 0; i < FFT_SIZE; i++) {
     i2s_read(I2S_NUM_0, &sample32, sizeof(sample32), &bytes_read, portMAX_DELAY);
 
-    // Convert microphone 32-bit PCM → float
-    float s = sample32 / 2147483648.0f;  // normalize to -1..1
+    float s = sample32 / 2147483648.0f;
 
     vReal[i] = s;
     vImag[i] = 0.0;
   }
 
-  // === Apply Hann window ===
   FFT.Windowing(vReal, FFT_SIZE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
 
-  // === FFT ===
   FFT.Compute(vReal, vImag, FFT_SIZE, FFT_FORWARD);
   FFT.ComplexToMagnitude(vReal, vImag, FFT_SIZE);
 
-  // === Find the highest peak ===
   double peakFreq = FFT.MajorPeak(vReal, FFT_SIZE, SAMPLE_RATE);
 
   Serial.print("Pitch: ");
