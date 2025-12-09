fc5c3bf9a43d032695ace024b607e7d36dafcb4a
Matt Franklin <mjfrankl@uci.edu>
2025-12-09 12:00:31 -0800

    Cloud Info

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "cloud_info.h"
+
+/*
+// Information from cloud_info.h:
+WIFI_SSID, WIFI_PASSWORD, SAS_TOKEN, root_ca
+iothubName, deviceName, url
+*/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
